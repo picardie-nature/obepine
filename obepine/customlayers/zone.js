9355608bc5d75bb2b145a0f4d494f57cce44f088
Agath21 <agathe.adam@jdev.fr>
2023-06-12 16:43:09 +0200

    Create home page and plugin to zoomToArea

---
+++ obepine/customlayers/zone.js
@@ -0,0 +1,24 @@
+
+const LAYER_URL = "apps/obepine/data/siteObepine_4326.geojson";
+const LAYER_ID = "zone";
+
+let layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: LAYER_URL,
+        format: new ol.format.GeoJSON()
+    }),
+    style: new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: "rgba(45, 64,89,255)",
+        width: 0.8
+      }),
+      fill: new ol.style.Fill({
+        color: "rgba(0, 0, 0, 0)",
+      }),
+    })
+});
+
+
+
+var zone = new CustomLayer(LAYER_ID, layer, legend);
+
